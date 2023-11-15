700a062d5e733007516108ed3b4e839242bb1ec3
Tim Batty <tbatty7@gmail.com>
2023-11-14 20:55:24 -0500

    add logs of objects and arrays to button press method

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
 function onClick(inputName) {
-    document.getElementById("output").innerHTML = "Now I am in control";
+    document.getElementById("displayedText").innerHTML = "Now I am in control";
     alert('Hello, ' + inputName + '!');
+    const user = {name: 'Bob'};
+    console.log('Hello ' + user.name);
+
+    const names = ['Sally', 'Jerome']
+    const users = [{name: 'Sally'}, {name: 'Jerome'}];
+    console.log('First name in list -----',names[0]);
+
+    console.log('Second name in list of objects -----',users[1].name);
 }
