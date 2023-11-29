4a3ef4e699fb0fd064aae2af97af9e70eea17bd0
Tim Batty <tbatty7@gmail.com>
2023-11-28 21:34:43 -0500

    transformed list into list of h2 tags with map

--- javascript.js
+++ javascript.js
@@ -3,14 +3,12 @@ function onPress(inputName) {
     const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
-            const pokemonNames = data.results.map(showNames)
+            const pokemonNames = data.results.map(pokemon => {
+                return '<h2>' + pokemon.name + '</h2>';
+            });
             console.log(pokemonNames);
-            document.getElementById("displayedText").innerHTML = pokemonNames.join(' ');
+            document.getElementById("displayedText").innerHTML = pokemonNames;
         }
     });
     
-}
-
-function showNames(pokemon) {
-    return pokemon.name
 }
