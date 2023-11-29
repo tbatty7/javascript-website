7b781caa3dfd32b376c2fe4bb1d92096ac2b6ebb
Tim Batty <tbatty7@gmail.com>
2023-11-28 21:23:50 -0500

    remove old code

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
 function onPress(inputName) {
-    document.getElementById("displayedText").innerHTML = "Now I am in control";
-    const user = {name: 'Bob'};
-    console.log('Hello ' + user.name);
-
-    const names = ['Sally', 'Jerome']
-    const users = [{name: 'Sally'}, {name: 'Jerome'}];
-    console.log('First name in list -----',names[0]);
-
-    console.log('Second name in list of objects -----',users[1].name);
+    
     const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
@@ -16,7 +8,6 @@ function onPress(inputName) {
             document.getElementById("displayedText").innerHTML = pokemonNames.join(' ');
         }
     });
-    alert('Hello, ' + inputName + '!');
     
 }
 
