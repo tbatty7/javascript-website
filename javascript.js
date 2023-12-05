8c552b48bef46d36a2af836db3fab0b59c8086c1
Tim Batty <tbatty7@gmail.com>
2023-12-05 14:14:51 -0500

    added pokemon name list wrapped in h2 tags

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 function onPress(inputName) {
     const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
-        console.log(status);
+        if (status === 'success') {
+            console.log(data);  
+            const pokemonNames = data.results.map(function(pokemon) {
+                return '<h2>' + pokemon.name + '</h2>'
+            });
+            document.getElementById('displayedText').innerHTML = pokemonNames;
+        }
     });
 }
