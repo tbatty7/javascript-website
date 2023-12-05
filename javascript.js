9ca23106535ec1365ef4f4862f7946ddc18c9b5d
Tim Batty <tbatty7@gmail.com>
2023-12-05 13:19:27 -0500

    cleaned up extra stuff

--- javascript.js
+++ javascript.js
@@ -1,16 +1,6 @@
 function onPress(inputName) {
-    document.getElementById("displayedText").innerHTML = "Now I am in control";
-    const user = {name: 'Bob'};
-    console.log('Hello ' + user.name);
-
-    const names = ['Sally', 'Jerome']
-    const users = [{name: 'Sally'}, {name: 'Jerome'}];
-    console.log('First name in list -----',names[0]);
-    console.log('Second name in list of objects -----',users[1].name);
     const url = 'https://pokeapi.co/api/v2/pokemon';
-    $.get(url,function(url, status){
+    $.get(url,function(data, status){
         console.log(status);
     });
-    alert('Hello, ' + inputName + '!');
-    
 }
