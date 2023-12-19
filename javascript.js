589905d8c80cff24a5d4fac6c7119e1667541b3e
Tim Batty <tbatty7@gmail.com>
2023-12-19 17:23:13 -0500

    use DOM to construct h2 tags and add plugins

--- javascript.js
+++ javascript.js
@@ -1,14 +1,15 @@
 function onPress(inputName) {
-    
-    const url = 'https://pokeapi.co/api/v2/pokemon';
-    $.get(url,function(data, status){
-        if (status === 'success') {
-            const pokemonNames = data.results.map(pokemon => {
-                return '<h2>' + pokemon.name + '</h2>';
-            });
-            console.log(pokemonNames);
-            document.getElementById("displayedText").innerHTML = pokemonNames;
-        }
+  const url = "https://pokeapi.co/api/v2/pokemon";
+  $.get(url, function (data, status) {
+      if (status === "success") {
+        const pokemonNames = data.results.forEach((pokemon) => {
+          const h2Element = document.createElement("h2");
+          h2Element.innerHTML = pokemon.name;
+          console.log("Name with H2 tag: ", h2Element);
+          document.getElementById("displayedText").append(h2Element);
+        });
+      } else {
+        console.log("Status was: " + status);
+      }
     });
-    
 }
