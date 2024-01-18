0d982fbb62d4fec6f8a40483443166359ee836b3
Tim Batty <tbatty7@gmail.com>
2024-01-18 13:59:58 -0500

    move adding element to correct place to fix error

--- javascript.js
+++ javascript.js
@@ -14,7 +14,6 @@ function addNamesToHTML(data) {
     const h2Element = document.createElement("h2");
     h2Element.innerHTML = pokemon.name;
     console.log("Name with H2 tag: ", h2Element);
+    document.getElementById("displayedText").append(h2Element);
   });
-
-  document.getElementById("displayedText").append(h2Element);
 }
