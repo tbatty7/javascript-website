cb7a77670ead1ab3b5dad757aec010320cc238b2
Tim Batty <tbatty7@gmail.com>
2024-01-18 12:14:23 -0500

    extract method

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 function onPress(inputName) {
   const url = "https://pokeapi.co/api/v2/pokemon";
   $.get(url, function (data, status) {
-      if (status === "success") {
-        const pokemonNames = data.results.forEach((pokemon) => {
-          const h2Element = document.createElement("h2");
-          h2Element.innerHTML = pokemon.name;
-          console.log("Name with H2 tag: ", h2Element);
-          document.getElementById("displayedText").append(h2Element);
-        });
-      } else {
-        console.log("Status was: " + status);
-      }
-    });
+    if (status === "success") {
+      addNamesToHTML(data);
+    } else {
+      console.log("Status was: " + status);
+    }
+  });
+}
+
+function addNamesToHTML(data) {
+  data.results.forEach((pokemon) => {
+    const h2Element = document.createElement("h2");
+    h2Element.innerHTML = pokemon.name;
+    console.log("Name with H2 tag: ", h2Element);
+  });
+
+  document.getElementById("displayedText").append(h2Element);
 }
