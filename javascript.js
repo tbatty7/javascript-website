67217ee8c2f28efc02938901204c9365b58e6618
Tim Batty <tbatty7@gmail.com>
2024-01-27 23:34:19 -0500

    did way too much in this commit - added images

--- javascript.js
+++ javascript.js
@@ -2,27 +2,70 @@ function onPress(inputName) {
   const url = "https://pokeapi.co/api/v2/pokemon";
   $.get(url, function (data, status) {
     if (status === "success") {
-      addNamesToHTML(data);
+      displayPokemonList(data);
     } else {
       console.log("Status was: " + status);
     }
   });
 }
 
-function addNamesToHTML(data) {
-  const pokemonList = data.results.map((pokemon) => {
+async function displayPokemonList(data) {
+  const pokemonList = data.results;
+  let pokemonListWithImages = await retrievePokemonImages(pokemonList);
+
+  buildHTML(pokemonListWithImages);
+}
+
+async function retrievePokemonImages(pokemonList) {
+  let pokemonListWithImages = [];
+  for (pokemon of pokemonList) {
+    const pokemonReference = { name: pokemon.name, image: "" };
+    await $.get(pokemon.url, (data, status) => {
+      if (status === "success") {
+        const pokemonImagUrl =
+          data.sprites.versions["generation-v"]["black-white"].animated
+            .front_default;
+        pokemonReference.image = pokemonImagUrl;
+        pokemonListWithImages.push(pokemonReference);
+        console.log(pokemonReference);
+      } else {
+        console.log("Failed to retrieve image for ", pokemon.name);
+      }
+    });
+  }
+  return pokemonListWithImages;
+}
+
+function buildHTML(pokemonList) {
+  const pokemonHtmlList = pokemonList.map((pokemon) => {
     const pokemonContainer = document.createElement("div");
     pokemonContainer.setAttribute(
       "style",
       "width: 250px; display: inline-block;" +
         "margin: 5px; background-color: cornflowerblue; border-radius: 30px;"
     );
-    const title = document.createElement("button");
-    title.innerHTML = pokemon.name;
-    pokemonContainer.append(title);
-    console.log("Name with H2 tag: ", pokemonContainer);
+    const titleButton = buildButtonTag(pokemon);
+    pokemonContainer.append(titleButton);
+
+    const image = buildImageTag(pokemon);
+    pokemonContainer.append(image);
 
     return pokemonContainer;
   });
-  $("#displayedText").html(pokemonList);
+  $("#displayedText").html(pokemonHtmlList);
 }
+
+function buildButtonTag(pokemon) {
+  const titleButton = document.createElement("button");
+  titleButton.setAttribute("onclick", () => { });
+  titleButton.innerHTML = pokemon.name;
+  return titleButton;
+}
+
+function buildImageTag(pokemon) {
+  const image = document.createElement("img");
+  image.setAttribute("src", pokemon.image);
+  image.setAttribute("style", "width: 100px; height: 100px;");
+  return image;
+}
+
